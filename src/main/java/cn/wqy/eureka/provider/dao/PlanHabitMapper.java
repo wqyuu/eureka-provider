37168113f80afcd6cf6fd36a86b18e27f1817404
wqyuu <2549067835@qq.com>
2019-01-04 17:53:14 +0800

    节点、计划dao

---
+++ src/main/java/cn/wqy/eureka/provider/dao/PlanHabitMapper.java
@@ -0,0 +1,9 @@
+package cn.wqy.eureka.provider.dao;
+
+import cn.wqy.eureka.provider.entity.PlanHabit;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanHabitMapper extends BaseMybatisDao<PlanHabit> {
+
+}
---
+++ src/main/java/cn/wqy/eureka/provider/dao/PlanNodeMapper.java
@@ -0,0 +1,9 @@
+package cn.wqy.eureka.provider.dao;
+
+import cn.wqy.eureka.provider.entity.PlanNode;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanNodeMapper extends BaseMybatisDao<PlanNode>{
+
+}
