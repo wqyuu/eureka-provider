fbc3e127000c7037e5d90519d2cf2e16e18a23cc
wqyuu <2549067835@qq.com>
2019-01-04 17:54:43 +0800

    初始导入

--- src/main/java/cn/wqy/eureka/provider/EurekaProviderApplication.java
+++ src/main/java/cn/wqy/eureka/provider/EurekaProviderApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * Created by wqy on 2019/01/03
  */
-@EnableDiscoveryClient //使用该注解将注册服务到eureka
+//@EnableDiscoveryClient //使用该注解将注册服务到eureka
 @SpringBootApplication
 @MapperScan("cn.wqy.eureka.provider.dao")//配置mybatis接口包扫描
+@EnableSwagger2
 public class EurekaProviderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
