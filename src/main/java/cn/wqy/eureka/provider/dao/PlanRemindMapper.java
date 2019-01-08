25810dcfaad52d29bdd934ef29f0d5e6f21475c3
wqyuu <2549067835@qq.com>
2019-01-08 09:51:48 +0800

    提醒dao层

---
+++ src/main/java/cn/wqy/eureka/provider/dao/PlanRemindMapper.java
@@ -0,0 +1,8 @@
+package cn.wqy.eureka.provider.dao;
+
+import cn.wqy.eureka.provider.entity.PlanRemind;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRemindMapper extends BaseMybatisDao<PlanRemind>{
+}
