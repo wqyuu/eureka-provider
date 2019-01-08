a9cc3e2aea7c9097826c45684c9b4839de5ddf40
wqyuu <2549067835@qq.com>
2019-01-08 10:09:31 +0800

    打开注册eureka注解

--- src/main/java/cn/wqy/eureka/provider/EurekaProviderApplication.java
+++ src/main/java/cn/wqy/eureka/provider/EurekaProviderApplication.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * Created by wqy on 2019/01/03
  */
-//@EnableDiscoveryClient //使用该注解将注册服务到eureka
+@EnableDiscoveryClient //使用该注解将注册服务到eureka
 @SpringBootApplication
 @MapperScan("cn.wqy.eureka.provider.dao")//配置mybatis接口包扫描
 @EnableSwagger2
